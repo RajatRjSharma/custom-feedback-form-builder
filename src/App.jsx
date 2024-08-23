416bccafef81a56bafb06bce514e116703f3856e
rajatrjsharma <merajatsharma@outlook.com>
2024-08-24 00:40:25 +0530

    First commit , additioon for material ui, react router dom

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const App = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Hello Vite + React!</h1>
+      <Button variant="contained" color="primary">
+        MUI Button
+      </Button>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import App from "./App.jsx";
+import "./index.css";
+
+const theme = createTheme();
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
