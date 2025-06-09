1921e3cc2cebbf86280585013555868e85dd8900
Rajat Sharma <merajatsharma@outlook.com>
2025-06-09 16:13:11 +0530

    update for vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/custom-feedback-form-builder/',
+  base: '/',
   plugins: [react()],
-  // base: '/custom-feedback-form-builder/'
-  base: '/'
 })
